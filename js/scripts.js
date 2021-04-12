94e847e7f91033c843e52d1b0df72f0311841248
Junior Silva <juniorsilvaphb@gmail.com>
2021-04-12 11:40:13 -0300

    version1

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/* Template: Starwars Front-End Challenge
+   Author: Junior Silva
+   Created: Apr 2021
+   Description: Custom JS file
+*/
+
+
+(function($) {
+    "use strict"; 
+	
+	/* Preloader */
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 500);
+		}
+		hidePreloader();
+	});
+
+})(jQuery);
